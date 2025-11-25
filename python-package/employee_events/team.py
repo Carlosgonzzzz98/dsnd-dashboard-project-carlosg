1bd7320f5fca931ff6c23da941b1ee13fe7a3440
Carlosgonzzzz98 <carlosgonzwork@gmail.com>
2025-11-25 11:01:03 -0700

    Update team.py

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,25 +1,21 @@
 # Import the QueryBase class
 from .query_base import QueryBase
-
 # Import dependencies for sql execution
 from .sql_execution import query
 
 # Create a subclass of QueryBase
 # called  `Team`
 class Team(QueryBase):
-
     # Set the class attribute `name`
     # to the string "team"
     name = "team"
-
-
+    
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
     @query
     def names(self):
-        
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
@@ -30,27 +26,24 @@ class Team(QueryBase):
             FROM team
         """
     
-
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
     @query
     def teamname(self, id):
-
         # Query 6
         # Write an SQL query
         # that selects the team_name column
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-         return f"""
+        return f"""
             SELECT team_name
             FROM team
             WHERE team_id = ({id})
         """
-
-
+    
     # Below is method with an SQL query
     # This SQL query generates the data needed for
     # the machine learning model.
@@ -60,16 +53,15 @@ class Team(QueryBase):
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
         return f"""
             SELECT positive_events, negative_events FROM (
-                    SELECT employee_id
-                         , SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                    GROUP BY employee_id
-                   )
-                """
+                SELECT employee_id
+                     , SUM(positive_events) positive_events
+                     , SUM(negative_events) negative_events
+                FROM {self.name}
+                JOIN employee_events
+                    USING({self.name}_id)
+                WHERE {self.name}.{self.name}_id = {id}
+                GROUP BY employee_id
+            )
+        """
