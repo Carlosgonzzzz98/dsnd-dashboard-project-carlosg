5ead7a257db45d5e1a839e704719edb5a72359d5
Carlosgonzzzz98 <carlosgonzwork@gmail.com>
2025-11-25 11:00:18 -0700

    Update employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,6 +1,5 @@
 # Import the QueryBase class
 from .query_base import QueryBase
-
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 from .sql_execution import query
@@ -8,19 +7,16 @@ from .sql_execution import query
 # Define a subclass of QueryBase
 # called Employee
 class Employee(QueryBase):
-
     # Set the class attribute `name`
     # to the string "employee"
     name = "employee"
-
-
+    
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
     @query
     def names(self):
-        
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -33,14 +29,12 @@ class Employee(QueryBase):
             FROM employee
         """
     
-
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     @query
     def username(self, id):
-        
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -52,8 +46,7 @@ class Employee(QueryBase):
             FROM employee
             WHERE employee_id = ({id})
         """
-
-
+    
     # Below is method with an SQL query
     # This SQL query generates the data needed for
     # the machine learning model.
@@ -63,12 +56,11 @@ class Employee(QueryBase):
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
         return f"""
-                    SELECT SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                """
+            SELECT SUM(positive_events) positive_events
+                 , SUM(negative_events) negative_events
+            FROM {self.name}
+            JOIN employee_events
+                USING({self.name}_id)
+            WHERE {self.name}.{self.name}_id = {id}
+        """
