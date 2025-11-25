9532a364d44c68a078f9a4bcaeb666b2f7ae6282
Carlosgonzzzz98 <carlosgonzwork@gmail.com>
2025-11-25 10:15:03 -0700

    Update query_base.py

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,28 +1,34 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from .sql_execution import query, db_path
+import pandas as pd
+import sqlite3
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
+class QueryBase:
 
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+        name = ""
 
     # Define a `names` method that receives
     # no passed arguments
-    # YOUR CODE HERE
+    @query
+    def names(self):
         
         # Return an empty list
-        # YOUR CODE HERE
-
+        return f"""
+            SELECT {self.name}_id, {self.name}_name
+            FROM {self.name}
+        """
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
-
+    def event_counts(self, id):
+        connection = sqlite3.connect(db_path)
+        
         # QUERY 1
         # Write an SQL query that groups by `event_date`
         # and sums the number of positive and negative events
@@ -31,13 +37,24 @@
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        # YOUR CODE HERE
+        df = pd.read_sql_query(f"""
+            SELECT event_date,
+                   SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
+            FROM employee_events
+            WHERE {self.name}_id = ({id})
+            GROUP BY event_date
+            ORDER BY event_date
+        """, connection)
+        connection.close()
+        return df
             
     
 
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
-    # YOUR CODE HERE
+    def notes(self, id):
+        connection = sqlite3.connect(db_path)
 
         # QUERY 2
         # Write an SQL query that returns `note_date`, and `note`
@@ -46,5 +63,12 @@
         # with f-string formatting
         # so the query returns the notes
         # for the table name in the `name` class attribute
-        # YOUR CODE HERE
+        df = pd.read_sql_query(f"""
+            SELECT note_date,
+                   note
+            FROM notes
+            WHERE {self.name}_id = ({id})
+        """, connection)
+        connection.close()
+        return df
 
